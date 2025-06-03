9a8e7314ccff60abdebd7d9a2614bdabc3473120
ASH1111111111111 <134459128+ASH1111111111111@users.noreply.github.com>
2025-06-03 03:35:48 +0000

    Jun 3, 2025, 3:35 AM

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
-# testing
+from random import randint
+
+def difficulty():
+    level_of_difficulty = input("What level would you like to chose? ")
+    return level_of_difficulty
+
+def bullett_chamber():
+    chamber = [0,0,0,0,0,0]
+    return chamber
+
+def number_of_bulletts(level_of_difficulty, chamber):
+    i = 1
+    b
+    while level_of_difficulty != True:
+        if level_of_difficulty == '1':
+            chamber.append(i, 1)
+        elif level_of_difficulty == '2':
+            for b in range(2):
+                chamber.append(i, 1)
+                i+=1
+        elif level_of_difficulty == '3':
+            for b in range(3):
+                chamber.append(i, 1)
+                i+=1
+        elif level_of_difficulty == '4':
+            for b in range(4):
+                chamber.append(i, 1)
+                i+=1
+        elif level_of_difficulty == '5':
+            for b in range(5):
+                chamber.append(i, 1)
+                i+=1
+        else:
+            print("That's not a number. ")
+            level_of_difficulty == False
+        
+
+
+
+def intro():
+    pass
+
+
+difficulty()
+number_of_bulletts()
