084c6bb06d1393ec52331bca07cd749735f72aea
ASH1111111111111 <134459128+ASH1111111111111@users.noreply.github.com>
2025-06-09 15:12:08 +0000

    I created a new funcion which allows me to output character by character in the terminal, ore metrovania look, I also fixed some minor errors such as spelling mistakes, and also fixed some code redundancy. I improved the main and chamber funcion

--- main.py
+++ main.py
@@ -1,107 +1,115 @@
 from time import sleep
 from random import randint
+from random import choice
+
+
+def print_letter_by_letter(text, delay=0.05):
+    for char in text:
+        print(char, end='', flush=True) # flush is used to ensure that the output is printed immediately
+        sleep(delay)
+    print()
 
 
 def greeting():
-    print("Hello There")
-    print("It seems that you've decided to challange me to a duel in russian roulete")
+    print_letter_by_letter("Hello There")
+    print_letter_by_letter("It seems that you've decided to challenge me to a duel in russian roulette")
     comfirm_begin = input("Is that true? ")
     if comfirm_begin.lower() == "yes":
-        print("Very well, let's begin")
+        print_letter_by_letter("Very well, let's begin")
     elif comfirm_begin.lower() == "no":
-        print("You intolerate bufoon, wasting my time. Back to where you come from")
+        print_letter_by_letter("You intolerate buffoon, wasting my time. Back to where you come from")
     else:
-        print("I don't speak this language wit thy tounge. Either say yes or no like a normal person would. You impertinent fool! ")
-        print("If you fail again, I'm going to banish you from this world.")
+        print_letter_by_letter("I don't speak this language wit thy tounge. Either say yes or no like a normal person would. You impertinent fool! ")
+        print_letter_by_letter("If you fail again, I'm going to banish you from this world.")
         
         bad_greeting()
         
 def bad_greeting():
-    print("So, will you continue and possibly shine amongst the light, or drown within the depths of the ocean.")
+    print_letter_by_letter("So, will you continue and possibly shine amongst the light, or drown within the depths of the ocean.")
     comfirm_begin = input("Are you going to battle me? ")
     if comfirm_begin.lower() == "yes":
-        print("Very well, took you a while but your tiny brain got there in the end.")
+        print_letter_by_letter("Very well, took you a while but your tiny brain got there in the end.")
     else:
-        print("To the streets of hell you go.")
-        print("*YOU HAVE BEEN BANISHED FROM THIS WORLD*")
+        print_letter_by_letter("To the streets of hell you go.")
+        print_letter_by_letter("*YOU HAVE BEEN BANISHED FROM THIS WORLD*")
         quit()
 
 
 def rules():
-    print("The rules of russian roulete is ⬇️")
-    print("1.You will be going against the master of russian roulete AKA me. ")
-    print("2. Tou can either hit, meaning you can shoot me or you can stand, which makes you shoot yourself")
-    print("3. We will go in turns. You will always go first as you are our guest. ")
-    print("4. You and I will both have 3 lives, everytime you get shot you will lose 1 life.")
-    print("5. First person to lose all their lives, loses.")
-    print("If you win, You will be held as king and I will be banished from this world. ")
-    print("If I win, you will be banished from this world and I will continue to rule over the land.")
-    print("----------------------------------------------------------------------------------------------------------------------------------------------")
+    print_letter_by_letter("The rules of russian roulette is ⬇️")
+    print_letter_by_letter("1.You will be going against the master of russian roulette AKA me. ")
+    print_letter_by_letter("2. Tou can either hit, meaning you can shoot me or you can stand, which makes you shoot yourself")
+    print_letter_by_letter("3. We will go in turns. You will always go first as you are our guest. ")
+    print_letter_by_letter("4. You and I will both have 3 lives, everytime you get shot you will lose 1 life.")
+    print_letter_by_letter("5. First person to lose all their lives, loses.")
+    print_letter_by_letter("If you win, You will be held as king and I will be banished from this world. ")
+    print_letter_by_letter("If I win, you will be banished from this world and I will continue to rule over the land.")
+    print("----------------------------------------------------------------------------------------------------------------")
     
 def terms_and_conditions():
     print("                                                                                             ")
     print("                                                                                             ")
-    print("Here are the terms and conditions ⬇️")
-    print("1. You must follow the rules of the game. Any rules not followed will result in an immediate ban. ")
-    print("2. Any thing that may happen to your device internally or externally will not be our fault.")
-    print("3. You must not impersonate anyone.")
-    print("4. Later depending on your further choices, somethings may change to your display")
-    print("5. Things may be said to you which may emotionally harm you, which could lead you to harm yourself phyically.")
-    print("This will not be ou fault. And please do not harm yourself. If you need help, please call +852 2343 2255 ")
-    print(" ")
-    print("----------------------------------------------------------------------------------------------------------------------------------------------")
-    print(" ")
-    print("Would you like to accept these terms of conditons? ")
+    print_letter_by_letter("Here are the terms and conditions ⬇️")
+    print_letter_by_letter("1. You must follow the rules of the game. Any rules not followed will result in an immediate ban. ")
+    print_letter_by_letter("2. Any thing that may happen to your device internally or externally will not be our fault.")
+    print_letter_by_letter("3. You must not impersonate anyone.")
+    print_letter_by_letter("4. Later depending on your further choices, somethings may change to your display")
+    print_letter_by_letter("5. Things may be said to you which may emotionally harm you, which could lead you to harm yourself phyically.")
+    print_letter_by_letter("This will not be ou fault. And please do not harm yourself. If you need help, please call +852 2343 2255 ")
+    print_letter_by_letter(" ")
+    print("----------------------------------------------------------------------------------------------------------------")
+    print_letter_by_letter(" ")
+    print_letter_by_letter("Would you like to accept these terms of conditons? ")
     proceed = input("Please answer as yes. Anything else entered will result in you being banished")
     if proceed.lower() == "yes":
         signature = input("Please sign here to comfirm. ")
-        print(f"Very well {signature}. Let's begin.")
+        print_letter_by_letter(f"Very well {signature}. Let's begin.")
     else:
-        print("*YOU HAVE BEEN BANISHED FROM THIS WORLD*")
+        print_letter_by_letter("*YOU HAVE BEEN BANISHED FROM THIS WORLD*")
         quit()
 
 def choose_mode():
     global mode_chosen
     
-    print("Now that you must choose a mode. Safe or Risky.")
-    print("----------------------------------------------------------------------------------------------------------------------------------------------")
-    print("Here are the differences.")
-    print("Safe mode")
-    print("In safe mode, If you win, your screen will flash rainbow for a few seonds only.")
-    print("But if you die. It will simply say that you've died.")
-    print("----------------------------------------------------------------------------------------------------------------------------------------------")
-    print("Risky mode")
-    print("I won't give so much detail, but all I will say the higher the difficulty, the higher reward but also the higher concequence.")
-    print("----------------------------------------------------------------------------------------------------------------------------------------------")
+    print_letter_by_letter("Now that you must choose a mode. Safe or Risky.")
+    print("----------------------------------------------------------------------------------------------------------------")
+    print_letter_by_letter("Here are the differences.")
+    print_letter_by_letter("Safe mode")
+    print_letter_by_letter("In safe mode, If you win, your screen will flash rainbow for a few seonds only.")
+    print_letter_by_letter("But if you die. It will simply say that you've died.")
+    print("----------------------------------------------------------------------------------------------------------------")
+    print_letter_by_letter("Risky mode")
+    print_letter_by_letter("I won't give so much detail, but all I will say the higher the difficulty, the higher reward but also the higher concequence.")
+    print("----------------------------------------------------------------------------------------------------------------")
     mode_chosen = input("So. What mode do you choose, safe or risky. ")
     if mode_chosen.lower() == "safe":
-        print("I see that you don't got the guts to play risky.")
-        print("But I guess I'll understand since you're just a mortal being.")
+        print_letter_by_letter("I see that you don't got the guts to play risky.")
+        print_letter_by_letter("But I guess I'll understand since you're just a mortal being.")
         sleep(0.7)
         safe()
         
     elif mode_chosen.lower() == "risky":
-        print("You've got guts")
-        print("And I like that.")
+        print_letter_by_letter("You've got guts")
+        print_letter_by_letter("And I like that.")
         sleep(0.7)
         risky()
     else:
-        print("Another idiot wasting my time. ")
-        print("*YOU HAVE BEEN BANISHED FROM THIS WORLD*")
+        print_letter_by_letter("Another idiot wasting my time. ")
+        print_letter_by_letter("*YOU HAVE BEEN BANISHED FROM THIS WORLD*")
         quit()
 
 def safe():
-    print("I see that you shiver in your boots")
-    print("But lets get into the game")
-    print("----------------------------------------------------------------------------------------------------------------------------------------------")
+    print_letter_by_letter("I see that you shiver in your boots")
+    print_letter_by_letter("But lets get into the game")
+    print("----------------------------------------------------------------------------------------------------------------")
     sleep(0.7)
 
     main_game()
 
 def risky():
-    print("I see you want to face me head on.")
-    print("I like that, let now continue the game.")
-    print("----------------------------------------------------------------------------------------------------------------------------------------------")
+    print_letter_by_letter("I see you want to face me head on.")
+    print_letter_by_letter("I like that, let now continue the game.")
+    print("----------------------------------------------------------------------------------------------------------------")
     sleep(0.7)
     
     main_game()
@@ -110,58 +118,60 @@ def main_game():
     global hit_or_stand
     global bot_lives
     global player_lives
-    print("I have already told you the rules of the games. ")
-    print("Now Let's begin")
+    print_letter_by_letter("I have already told you the rules of the games. ")
+    print_letter_by_letter("Now Let's begin")
     
     
     
     bot_lives = 3
     player_lives = 3
     
-    the_chamber = 6
+    the_chamber = ["blank"] * 6
     
     valid_input = False
     hit_or_stand = input("Do you want to hit or stand")
     if hit_or_stand.lower() == "hit":
         valid_input = True
-        print("Wanna just play it safe, fair enough")
+        print_letter_by_letter("Wanna just play it safe, fair enough")
     elif hit_or_stand.lower() == "stand":
         valid_input = True
-        print("Wanna play it risky eh")
+        print_letter_by_letter("Wanna play it risky eh")
     else: 
         while valid_input == False:
-            print("Just say either 'hit' or 'stand' you idiot you bufoon")
+            print_letter_by_letter("Just say either 'hit' or 'stand' you idiot you buffoon")
             hit_or_stand = input("Do you want to hit or stand")
     
+    chamber_result = chamber(the_chamber)
+    resolve_outcome(chamber_result)
 
-def chamber():
 
-    chamber == "blank","blank","blank","blank","blank","blank"
-    for bullets in chamber:
-        if bullets == "blank":
-            chamber = "blank"
-        elif bullets == "full":
-            chamber = "full"
-    print("The chamber has been loaded")
-    if chamber == "full" and hit_or_stand.lower() == "hit":
-        print("You shot me, that was just luck")
-        bot_lives - 1
-    elif chamber == "full" and hit_or_stand.lower() == "stand":
-        print("You shot yourself, you fool")
-        player_lives - 1
-    elif chamber == "blank" and hit_or_stand.lower() == "stand":
-        print("You stood, nothing happened, little lucky son of a biscuit")
-    elif chamber == "blank" and hit_or_stand.lower() == "hit":
-        print("You hit me, but I was lucky ... I mean skillful and didn't get shot")
+def chamber(the_chamber):
+
+    bullet_position = choice(range(6))
+    the_chamber[bullet_position] = "full"
+
+    print_letter_by_letter("The chamber has been loaded")
+    return the_chamber[bullet_position]
+
+def resolve_outcome(chamber_result):
+    global bot_lives
+    global player_lives
+    global hit_or_stand
+    
+    if chamber_result == "full" and hit_or_stand.lower() == "hit":
+        print_letter_by_letter("You shot me, that was just luck")
+        bot_lives -= 1
+    elif chamber_result == "full" and hit_or_stand.lower() == "stand":
+        print_letter_by_letter("You shot yourself, you idiot")
+        player_lives -= 1
+    elif chamber_result == "blank" and hit_or_stand.lower() == "hit":
+        print_letter_by_letter("You hit me, but I was lucky ... I mean skillful and didn't get shot.")
+    elif chamber_result == "blank" and hit_or_stand.lower() == "stand":
+        print_letter_by_letter("You stood, nothing happened, little lucky son of a biscuit.")
     else:
-        print("Something went wrong, you must have cheated, you fool")
-        print("*YOU HAVE BEEN BANISHED FROM THIS WORLD*")
+        print_letter_by_letter("Something went wrong, you idiot. You should never have come here in the first place.")
+        print_letter_by_letter("*YOU HAVE BEEN BANISHED FROM THIS WORLD*")
         quit()
-            
-     
-    
-    
-
 
 
 
