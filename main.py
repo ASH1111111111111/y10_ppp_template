6d499e0c56d4e8c3b2dc841fdc038fe00660b752
ASH1111111111111 <134459128+ASH1111111111111@users.noreply.github.com>
2025-06-23 13:13:35 +0000

    Jun 23, 2025, 1:13 PM

--- main.py
+++ main.py
@@ -233,7 +233,7 @@ def player_turn():
         if hit_or_stand.lower() in ["hit", "stand"]:
             valid_input = True
             chamber_result = chamber()
-            resolve_outcome(chamber_result, hit_or_stand.lower())
+            resolve_outcome(chamber_result, hit_or_stand.lower(), is_player_turn=())
         else:
             print_letter_by_letter("Just say hit or stand you idiot")
 
@@ -248,7 +248,7 @@ def bot_turn():
         print_letter_by_letter("I have choosen to stand, I know I'm safe")
     
     chamber_result = chamber()
-    resolve_outcome(chamber_result, hit_or_stand)
+    resolve_outcome(chamber_result, hit_or_stand, False)
    
 
         
